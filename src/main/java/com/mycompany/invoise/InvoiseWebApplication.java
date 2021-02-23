d719bc671b0e9f69b4bb11a2e6090c4c524e45fd
Martinez <martinez.forestier@gmail.com>
2021-02-23 16:11:14 +0100

    add jdbc config and repo data link from DataBase 'INVOICE'

--- src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
+++ src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
@@ -7,13 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InvoiseWebApplication {
 
 	public static void main(String[] args) {
-
-		/*ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(App.class);
-		ApplicationContext applicationContext = SpringApplication.run(App.class, args);
-		InvoiceControllerInterface invoiceControllerInterface = applicationContext.getBean(InvoiceControllerInterface.class);//soit par l'id du Bean
-		invoiceControllerInterface.createInvoice();*/
-
 		SpringApplication.run(InvoiseWebApplication.class, args);
 	}
 
