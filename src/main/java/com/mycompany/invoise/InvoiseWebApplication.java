c0cb6252ae6c2f9d4f989441787e85b2df567af5
Martinez <martinez.forestier@gmail.com>
2021-02-25 12:18:43 +0100

    Spring Data Jpa (with Hibernate)

--- src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
+++ src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
@@ -1,9 +1,7 @@
 package com.mycompany.invoise;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
 public class InvoiseWebApplication {
